8e0b6c90770da9b78bcdad0190ec254eebd53187
Abel <abeladdokoh11@gmail.com>
2022-06-08 15:11:27 -0700

    I sometimes suffer from insomnia

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10t -> prints the lowercase alphabets
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ *
+ * Return: Always 0 (Success)
  */
-
 void print_alphabet_x10(void)
 {
-int j;
-int n;
+int a, b;
 
-for (n = 0; n < 10; n++)
+for (a = 0; a <= 9; a++)
 {
-for (j = 'a'; j <= 'z'; j++)
+for (b = 97; b <= 122; b++)
 {
-_putchar(j);
+_putchar(b);
 }
 _putchar('\n');
 }
